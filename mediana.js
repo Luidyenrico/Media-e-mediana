dea8c5e7a371fe89b7b0c9ecf9e32a2f0eb258e2
Luidy Enrico <80763934+Luidyenrico@users.noreply.github.com>
2023-04-24 16:13:54 -0300

    Add files via upload

--- mediana.js
+++ mediana.js
@@ -1,36 +1,37 @@
-let botao = document.getElementById("botaoCalcular")
+let botaoCal = document.getElementById("botaoCalcular")
+let botaoInf = document.getElementById("botaoInfo")
 
-botao.addEventListener("mouseover", Mostra_Info);
-botao.addEventListener("mouseout", Esconde_Info);
-botao.addEventListener("click", Calcular);
-
-
+botaoInf.addEventListener("mouseover", Mostra_Info);
+botaoInf.addEventListener("mouseout", Esconde_Info);
+botaoCal.addEventListener("click", Calcular);
 
 function Calcular() {
     let numeroA = document.getElementById("inputNumA")
     let numeroB = document.getElementById("inputNumB")
-    let valoresAB = [Number(numeroA.value), Number(numeroB.value)]
     let divRes = document.getElementById("divResultado")
-    divRes.innerHTML = valoresAB
-}
+        let valorA = Number(numeroA.value)
+        let valorB = Number(numeroB.value)
+
+    if (numeroA.value.length == 0 || numeroB.value.length == 0){
+        divRes.innerHTML = "Digite um valor válido nas duas caixas."
+            } else {
+             if (valorA < valorB) {
+                let resultadoC = 2 * valorA - valorB
+                divRes.innerHTML = `<strong>C</strong> = ${resultadoC}`
+            } else {
+                let resultadoC = 2 * valorB - valorA
+                divRes.innerHTML = `<strong>C</strong> = ${resultadoC}`
+            }
+            }
+        }
+
 
 function Mostra_Info() {
-
 let informacao = document.getElementById("sectioninformacoes")
 informacao.style.display = "block"
-
-
-
 }
 
 function Esconde_Info() {
-
     let informacao = document.getElementById("sectioninformacoes")
     informacao.style.display = "none"
-
 }
-
-
-
-
-
