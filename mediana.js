402b8f0ba4a8f4465e7b929f8c8cd5246739a955
Luidy Enrico <80763934+Luidyenrico@users.noreply.github.com>
2023-04-30 22:58:28 -0300

    Add files via upload

--- mediana.js
+++ mediana.js
@@ -1,24 +1,24 @@
 let botaoCal = document.getElementById("botaoCalcular")
 let botaoInf = document.getElementById("botaoInfo")
 
-botaoInf.addEventListener("mouseover", Mostra_Info);
-botaoInf.addEventListener("mouseout", Esconde_Info);
-botaoCal.addEventListener("click", Calcular);
+botaoInf.addEventListener("mouseover", Mostra_Info); // Vai adicionar a função de mostrar informação ao botão info quando passar o mouse.
+botaoInf.addEventListener("mouseout", Esconde_Info); // Vai sumir com as informações ao tirar o mouse.
+botaoCal.addEventListener("click", Calcular); // Atribui a função ao Botão principal de calcular.
 
-function Calcular() {
+function Calcular() { // Função para receber os valores e realizar o calculo.
     let numeroA = document.getElementById("inputNumA")
     let numeroB = document.getElementById("inputNumB")
     let divRes = document.getElementById("divResultado")
         let valorA = Number(numeroA.value)
         let valorB = Number(numeroB.value)
 
-    if (numeroA.value.length == 0 || numeroB.value.length == 0){
+    if (numeroA.value.length == 0 || numeroB.value.length == 0){ // if para caso não seja colocado nenhum valor, não dar erro.
         divRes.innerHTML = "Digite um valor válido nas duas caixas."
             } else {
-             if (valorA < valorB) {
+             if (valorA < valorB) { // Esse if é para sempre funcionar independente da ordem que o usuario coloque os numeros para que sempre o numero do meio seja a mediana.
                 let resultadoC = 2 * valorA - valorB
                 divRes.innerHTML = `<strong>C</strong> = ${resultadoC}`
-            } else {
+            } else {  
                 let resultadoC = 2 * valorB - valorA
                 divRes.innerHTML = `<strong>C</strong> = ${resultadoC}`
             }
@@ -26,12 +26,12 @@ function Calcular() {
         }
 
 
-function Mostra_Info() {
+function Mostra_Info() { // Função de mostrar informações do botão de info.
 let informacao = document.getElementById("sectioninformacoes")
 informacao.style.display = "block"
 }
 
-function Esconde_Info() {
+function Esconde_Info() { // Função de esconder informações do botão de info.
     let informacao = document.getElementById("sectioninformacoes")
     informacao.style.display = "none"
 }
